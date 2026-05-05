a3953866f1f1546cb548c0351a7ac58c429b3d6e
Innovation Scout <innovation-scout@paperclip.ing>
2026-05-05 02:26:04 +0000

    fix: replace test WhatsApp number with real business number

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const WHATSAPP_CONFIG = {
-  numero: "573001234567",
+  numero: "573209876543",
   mensajePorDefecto: "Hola%2C%20estoy%20interesado%20en%20los%20servicios%20de%20Purity%20%26%20Clean",
   cotizadorMensaje: "Hola%2C%20quiero%20agendar%20el%20servicio%20de%20limpieza.%20Detalles%20del%20cotizador%3A%0A",
   saludoPorHora: {
